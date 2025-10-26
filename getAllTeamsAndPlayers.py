d3229c3176e62b8dd61995c2c6abf904c0f88a92
miko <84585119+m1k0103@users.noreply.github.com>
2025-10-26 13:48:37 +0000

    init script
    init script

---
+++ getAllTeamsAndPlayers.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from multiprocessing import Pool, cpu_count
+
+
+def getTeamLeaderboard(pageNum):
+    url = f"https://tournaments.nse.gg/tournaments/counter-strike-2-nse-winter-25/teams/sort/name?page={pageNum}"
+    
+    raw_content = requests.get(url=url).text
+    soup = BeautifulSoup(raw_content, "html.parser")
+
+    teamnamesElements = soup.find_all("td", {"class": "teams-name"})
+    allTeamNames = []
+    for i in range(len(teamnamesElements)):
+        s2 = BeautifulSoup(str(soup.find_all("td", {"class": "teams-name"})[i]), "html.parser")
+        allTeamNames.append([s2.find("a").get_text().strip(), s2.find("a")["href"]])
+    
+    return allTeamNames #returns in 3d array in the following format [[name,url], ...]
+
+
+def getTeamPlayers(teamurl):
+    url = f"https://tournaments.nse.gg/{teamurl}"
+    
+    raw_text = requests.get(url=url).text
+    soup = BeautifulSoup(raw_text, "html.parser")
+
+
+    allPlayerData = []
+    #get all player names, profile_urls and their role
+    for playerHtml in soup.find_all("tr", {"class":"odd"}) + soup.find_all("tr", {"class":"even"}):
+        playerSoup = BeautifulSoup(str(playerHtml), "html.parser")
+
+        playerUrl = playerSoup.find("a")["href"]
+        playerName = playerSoup.find("a").text.strip()
+        playerRole = "Leader" if "Leader" in str(playerSoup) else "Player"
+        allPlayerData.append([playerName, playerUrl, playerRole])
+        print(playerName, playerUrl, playerRole)
+    return allPlayerData # returns in format [[name,url,role], [...], ...]
+    
+
+def writePlayerDataToFile(data):
+    with open("players.txt", "a") as f:
+        f.write(f"{data[0][0]} | {data[0][1]} | {data[0][2]}\n")
+    return
+
+
+
+if __name__ == "__main__":
+    # gets all teams from a leaderboard page
+    all_teams = []
+    for i in range(6):
+        all_teams.extend(getTeamLeaderboard(i))
+
+    # get only team urls and feed them into func which gets all players in each team
+    team_urls = [i[1] for i in all_teams]
+    with Pool(cpu_count()) as p:
+        results = p.map(getTeamPlayers, team_urls)
+
+    # write all players to file
+    for i in results:
+        writePlayerDataToFile(i)
+
+
+    getTeamPlayers(team_urls[0])
